51fbe9bf790e2e135db1dd118a961afb5b7478f4
creativcoder <rsconceptx@gmail.com>
2016-01-31 22:46:32 +0530

    initial setup of markdown editor

---
+++ public/script.js
@@ -0,0 +1,17 @@
+window.onload = function() {
+    var converter = new showdown.Converter();
+    var pad = document.getElementById('pad');
+    var markdownArea = document.getElementById('markdown');
+
+    var convertMarkdownArea = function() {
+        var markdown_text = pad.value;
+        html = converter.makeHtml(markdown_text);
+        markdownArea.innerHTML = html;
+    };
+
+    pad.addEventListener('input',convertMarkdownArea);
+
+    convertMarkdownArea();
+
+      
+};
